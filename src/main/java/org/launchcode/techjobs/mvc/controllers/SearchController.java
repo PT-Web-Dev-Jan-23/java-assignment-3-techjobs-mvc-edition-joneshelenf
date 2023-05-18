f62a5996ded575f41a4224344a80a14f42956420
Helen Jones <helenfrancesjones@gmail.com>
2023-05-18 10:28:44 -0500

    post mapping instead of request mapping for TODO 3

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -5,10 +5,12 @@ import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -27,15 +29,33 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @RequestMapping(value = "results")
-    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
-        ArrayList<Job> jobs;
-        jobs = JobData.findByColumnAndValue(searchType, searchTerm);
-        model.addAttribute("type", searchType);
-        model.addAttribute("columns", columnChoices);
-        model.addAttribute("title", "Search Condition: " + columnChoices.get(searchType) + " Search Term: " + searchTerm);
+    @PostMapping("/search")
+    public String displaySearchResults(Model model, @RequestParam("searchType") String searchType, @RequestParam("searchTerm") String searchTerm) {
+        List<Job> jobs;
+
+        if (searchTerm.equals("all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
         model.addAttribute("jobs", jobs);
-        return "search.html";
+        model.addAttribute("columns", ListController.columnChoices);
+
+        return "search";
     }
 }
+
+
+//    @RequestMapping(value = "results")
+//    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+//        ArrayList<Job> jobs;
+//        jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+//        model.addAttribute("type", searchType);
+//        model.addAttribute("columns", columnChoices);
+//        model.addAttribute("title", "Search Condition: " + columnChoices.get(searchType) + " Search Term: " + searchTerm);
+//        model.addAttribute("jobs", jobs);
+//        return "search.html";
+//    }
+
 
